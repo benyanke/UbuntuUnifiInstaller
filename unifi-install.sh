99d2ae513de763f466384ac64ea7b514b4675fc5
Ben Yanke <ben@benyanke.com>
2016-07-13 11:05:09 -0500

    Update unifi-install.sh

--- unifi-install.sh
+++ unifi-install.sh
@@ -52,6 +52,10 @@ dialog  --backtitle "$backTitleText" \
 --title "Domain" \
 --inputbox "\nWhat domain name do you wish to use (ex: example.com)?\n" 0 0  2> $tempfile3
 
+
+cat $tempfile3 | grep -P "^[a-zA-Z0-9]+([-.]?[a-zA-Z0-9]+)*.[a-zA-Z]+$"
+echo $eq
+
 # dev testing
 domain=$(cat $tempfile3)
 
