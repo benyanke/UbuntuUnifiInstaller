3cc60af2f25765ab69b0486a6cc5b01691e3dcef
Ben Yanke <ben@benyanke.com>
2016-07-13 11:37:45 -0500

    Update unifi-install.sh

--- unifi-install.sh
+++ unifi-install.sh
@@ -78,6 +78,14 @@ fi ## end domain check
 dialog  --backtitle "$backTitleText" \
 --title "Port" \
 --inputbox "\nWhat port do you wish to use?\n\n" 0 0  2> $tempfile5
+  
+  # is port valid
+  if [ $(cat $tempfile5) -lt 1 &&  $(cat $tempfile5) -gt 65535]; then
+      dialog  --backtitle "$backTitleText" \
+      --title "Port Not Valid" \
+      --infobox "\n*$(cat $tempfile5)* does not appear to be a valid port. Exiting.\n\n" 0 0 
+      exit 1;
+  fi ## end domain validity check
 
 dialog  --backtitle "$backTitleText" \
 --title "Confirmation" \
