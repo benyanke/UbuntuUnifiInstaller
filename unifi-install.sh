5c03b0d3b6a063149a2e89fea576b2bba7e340a3
Ben Yanke <ben@benyanke.com>
2016-07-13 18:05:51 -0500

    Update unifi-install.sh

--- unifi-install.sh
+++ unifi-install.sh
@@ -151,8 +151,8 @@ wait;
 
 
 # Generate stronger DH groups
-# openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048  > /dev/null 2>&1 &
-openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048 &
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048  > /dev/null 2>&1 &
+# openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048 &
 
 # Setup nginx to proxy to unifi
 # Let's encrypt certificate
